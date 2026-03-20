abe1e624adba0646d3db09b8a50aeb2cf91e306f
Pavi532 <udara.sync@gmail.com>
2026-03-20 14:49:04 +0530

    Updated main.js

--- src/main.js
+++ src/main.js
@@ -11,19 +11,18 @@ const paraY = document.getElementById('y')
 const paraZ = document.getElementById('z')
 const btnPermission = document.getElementById('btnPermission')
 
-btnPermission.addEventListener("click", async() => {
-  alert('clicked')
+btnPermission.addEventListener("click", (e) => {
+  e.preventDefault()
   if (typeof window.DeviceMotionEvent.requestPermission !== "function") {
     // The feature is not available, or does not need permission.
     gyroDiv.innerHTML = "Not Supported"
     return;
   }
-  gyroDiv.innerHTML = DeviceMotionEvent.requestPermission();
 
-  const permission = await DeviceMotionEvent.requestPermission();
+  const permission = DeviceMotionEvent.requestPermission();
   gyroDiv.innerHTML = permission
   if (permission === "granted"){
-    
+    alert("granted")
     window.addEventListener("devicemotion", (event) => {
       console.log(`${event.acceleration.x} m/s2`);
       console.log(`${event.acceleration.y} m/s2`);
