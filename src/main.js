9a31bbab26458a65b01fc69c0897d6ab0ca936eb
Pavi532 <udara.sync@gmail.com>
2026-03-20 10:57:19 +0530

    first commit

---
+++ src/main.js
@@ -0,0 +1,38 @@
+import './style.css'
+import javascriptLogo from './assets/javascript.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import { setupCounter } from './counter.js'
+
+
+const gyroDiv = document.getElementById('gyrodiv');
+const paraX = document.getElementById('x')
+const paraY = document.getElementById('y')
+const paraZ = document.getElementById('z')
+const btnPermission = document.getElementById('btnPermission')
+
+btnPermission.addEventListener("click", async() => {
+  alert('clicked')
+  if (typeof window.DeviceMotionEvent.requestPermission !== "function") {
+    // The feature is not available, or does not need permission.
+    gyroDiv.innerHTML = "Not Supported"
+    return;
+  }
+  gyroDiv.innerHTML = DeviceMotionEvent.requestPermission();
+
+  const permission = await DeviceMotionEvent.requestPermission();
+  gyroDiv.innerHTML = permission
+  if (permission === "granted"){
+    
+    window.addEventListener("devicemotion", (event) => {
+      console.log(`${event.acceleration.x} m/s2`);
+      console.log(`${event.acceleration.y} m/s2`);
+      console.log(`${event.acceleration.z} m/s2`);
+    
+     paraX.innerText = `${event.acceleration.x} m/s2`;
+     paraY.innerText = `${event.acceleration.y} m/s2`;
+     paraZ.innerText = `${event.acceleration.z} m/s2`;
+      
+    });
+  }
+});
